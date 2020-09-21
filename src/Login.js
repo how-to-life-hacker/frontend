ff22905c0d7851df83a48a5316727859f3c5dce2
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-09-21 13:02:15 -0700

    Fixed Errors

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./Forms.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div className="login" className="main-form">
+    <div className="login">
       <div className="form-links">
         <Link to="/login">Login </Link>
         <Link to="/signup">Signup</Link>
--- src/Signup.js
+++ src/Signup.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Forms.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
 
 export default function Forms() {
   return (
-    <div className="signup" className="main-form">
+    <div className="signup">
       <div className="form-links">
         <Link to="/login">Login </Link>
         <Link to="/signup">Sign Up</Link>
